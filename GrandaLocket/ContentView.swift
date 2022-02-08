686245ca2c4ef71ccbfd31fd953b107beac7b438
Alexey Salangin <a7ls@ya.ru>
2022-02-08 09:47:16 +0100

    Initial Commit

---
+++ GrandaLocket/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GrandaLocket
+//
+//  Created by Alexey Salangin on 08.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GrandaLocket/GrandaLocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrandaLocketApp.swift
+//  GrandaLocket
+//
+//  Created by Alexey Salangin on 08.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct GrandaLocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
