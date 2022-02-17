ea47e4d2f425fcdb7666afdb9fc9c8a01a4212e8
Oneanya <aaw98@ya.ru>
2022-02-17 15:55:29 +0100

    Add Palette

---
+++ GrandaLocket/DesignKit/Palette.swift
@@ -0,0 +1,22 @@
+//
+//  Palette.swift
+//  GrandaLocket
+//
+//  Created by Аня on 16/02/22.
+//
+
+import SwiftUI
+
+enum Palette {
+    static let blackHard = Color(rgb: 0x110E15)
+    static let blackMiddle = Color(rgb: 0x252525)
+    static let blackHight = Color(rgb: 0x6B6B6B)
+    static let blackOpacity = Color(rgb: 0x000000).opacity(0.4)
+
+    static let whiteHard = Color(rgb: 0xFFFFFF)
+    static let whiteMiddle = Color(rgb: 0xFFFFFF).opacity(0.8)
+    static let whiteLight = Color(rgb: 0xFFFFFF).opacity(0.6)
+    static let whiteOpacity = Color(rgb: 0xFFFFFF).opacity(0.06)
+
+    static let accent = UIColor(rgb: 0xCFF308)
+}
